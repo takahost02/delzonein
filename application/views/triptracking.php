6baae24e4562c8fbe8a0ceaa529aacc75a819622
takahost02 <takahost02@gmail.com>
2025-07-11 17:16:41 +0530

    ik

--- application/views/triptracking.php
+++ application/views/triptracking.php
@@ -50,10 +50,9 @@
 <script src="<?= base_url(); ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>assets/plugins/toast/toast.min.css" />
  <script src="<?= base_url(); ?>assets/plugins/toast/toast.min.js"></script>
- <script
+ <script   async defer
   src="https://maps.googleapis.com/maps/api/js?key=<?= $data['s_googel_api_key']; ?>&callback=initMap&libraries=places,drawing&v=weekly"
-  async
-  defer
+
 ></script>
 <script type="text/javascript" src="https://www.google.com/jsapi"></script>
 <script id="group"  src="<?php echo base_url(); ?>assets/triptracking.js"></script>

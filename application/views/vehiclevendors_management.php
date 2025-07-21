47285c82bd6abe65de9afdde23fa4626529996ff
DIGITYX <digismartskills12@gmail.com>
2025-07-21 12:42:22 +0530

    i

--- application/views/vehiclevendors_management.php
+++ application/views/vehiclevendors_management.php
@@ -40,39 +40,49 @@
                      </tr>
                   </thead>
                   <tbody>
-                       <?php if(!empty($vehiclevendorslist)){  $count=1;
-                        foreach($vehiclevendorslist as $vehiclevendorslist){
-                        ?>
-                     <tr>
-                        <td> <?php echo output($count); $count++; ?></td>
-                        <td> <?php echo output($vehiclevendorslist['vn_name']); ?>
+                     <?php if (!empty($vehiclevendorslist)) {
+                        $count = 1;
+                        foreach ($vehiclevendorslist as $vehiclevendorslist) {
+                     ?>
+                           <tr>
+                              <td> <?php echo output($count);
+                                    $count++; ?></td>
+                              <td> <?php echo output($vehiclevendorslist['vn_name']); ?>
 
-                        <td> <?php echo output($vehiclevendorslist['vn_contact_person']); ?>
-                        <td> <?php echo output($vehiclevendorslist['vn_mobile']); ?></td>
-                       
-                        <td><?php  if($vehiclevendorslist['vn_doj']!='') {  echo output(date(dateformat(), strtotime($vehiclevendorslist['vn_doj']))); } else { echo '-'; } ?></td>
-                        <td><?php if($vehiclevendorslist['vn_file']!='') { ?>
-                        <a target="_blank" href="<?= base_url(); ?>assets/uploads/<?= ucwords($vehiclevendorslist['vn_file']); ?>" class="">
-                          View
-                        </a>
-                        <?php } else { echo '-'; } ?></td>
-                        <td> <?php echo output($vehiclevendorslist['vn_address']); ?></td>
-                        <td>  <span class="badge <?php echo ($vehiclevendorslist['vn_is_active']=='1') ? 'badge-success' : 'badge-danger'; ?> "><?php echo ($vehiclevendorslist['vn_is_active']=='1') ? 'Active' : 'Inactive'; ?></span>  </td>
-                        <td>
-                           <a class="icon" href="<?php echo base_url(); ?>vehiclevendors/editvehiclevendor/<?php echo output(encodeId($vehiclevendorslist['vn_id'])); ?>">
-                           <i class="fa fa-edit"></i>
-                           </a>
-                           <?php  if(userpermission('lr_vehiclevendors_del')) { ?>
+                              <td> <?php echo output($vehiclevendorslist['vn_contact_person']); ?>
+                              <td> <?php echo output($vehiclevendorslist['vn_mobile']); ?></td>
 
-                              <a data-toggle="modal" href="" onclick="confirmation('<?php echo base_url(); ?>conductors/deleteconductor','<?= output($vehiclevendorslist['vn_id']); ?>')" data-target="#deleteconfirm" class="icon text-danger" data-toggle="tooltip" data-placement="top"><i class="fa fa-trash"></i></a>
-                           </a> 
-                           <?php } ?>
-                        </td>
-                        <?php } } ?>
-                     </tr>
+                              <td><?php if ($vehiclevendorslist['vn_doj'] != '') {
+                                       echo output(date(dateformat(), strtotime($vehiclevendorslist['vn_doj'])));
+                                    } else {
+                                       echo '-';
+                                    } ?></td>
+                              <td><?php if ($vehiclevendorslist['vn_file'] != '') { ?>
+                                    <a target="_blank" href="<?= base_url(); ?>assets/uploads/<?= ucwords($vehiclevendorslist['vn_file']); ?>" class="">
+                                       View
+                                    </a>
+                                 <?php } else {
+                                       echo '-';
+                                    } ?>
+                              </td>
+                              <td> <?php echo output($vehiclevendorslist['vn_address']); ?></td>
+                              <td> <span class="badge <?php echo ($vehiclevendorslist['vn_is_active'] == '1') ? 'badge-success' : 'badge-danger'; ?> "><?php echo ($vehiclevendorslist['vn_is_active'] == '1') ? 'Active' : 'Inactive'; ?></span> </td>
+                              <td>
+                                 <a class="icon" href="<?php echo base_url(); ?>vehiclevendors/editvehiclevendor/<?php echo output(encodeId($vehiclevendorslist['vn_id'])); ?>">
+                                    <i class="fa fa-edit"></i>
+                                 </a>
+                                 <?php if (userpermission('lr_vehiclevendors_del')) { ?>
+
+                                    <a data-toggle="modal" href="" onclick="confirmation('<?php echo base_url(); ?>vehiclevendors/deleteconductor','<?= output($vehiclevendorslist['vn_id']); ?>')" data-target="#deleteconfirm" class="icon text-danger" data-toggle="tooltip" data-placement="top"><i class="fa fa-trash"></i></a>
+                                    </a>
+                                 <?php } ?>
+                              </td>
+                        <?php }
+                     } ?>
+                           </tr>
                   </tbody>
                </table>
-              
+
             </div>
          </div>
          <!-- /.card-body -->

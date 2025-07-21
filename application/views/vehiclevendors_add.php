775366f024383783ef01c226932f98fef3c9c1a7
DIGITYX <digismartskills12@gmail.com>
2025-07-21 11:05:43 +0530

    y

--- application/views/vehiclevendors_add.php
+++ application/views/vehiclevendors_add.php
@@ -85,7 +85,7 @@
           <div class="col-sm-6 col-md-3">
             <div class="form-group">
               <label class="form-label">Password<span class="form-required">*</span></label>
-              <input type="text" name="vn_password" value="<?php echo (isset($vehiclevendordetails)) ? $vehiclevendordetails[0]['vn_password'] : '' ?>" class="form-control" placeholder="password" />
+              <input type="text" name="vn_password" class="form-control" placeholder="password" />
             </div>
           </div>
 

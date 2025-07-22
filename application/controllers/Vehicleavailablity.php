d9c7cc2c49fc60d4518e6f78344e8a90331df748
takahost02 <takahost02@gmail.com>
2025-07-22 15:48:49 +0530

    i

--- application/controllers/Vehicleavailablity.php
+++ application/controllers/Vehicleavailablity.php
@@ -25,13 +25,12 @@ class Vehicleavailablity extends CI_Controller
 			$this->db->where('t_driver', $user_id);
 			$vehicle_ids = array_column($this->db->get()->result_array(), 't_vechicle');
 		} elseif ($user_type == 'vendor') {
-			$this->db->select('trips.t_vechicle');
-			$this->db->from('trips');
-			$this->db->join('vehicles', 'trips.t_vechicle = vehicles.v_id');
-			$this->db->where('vehicles.v_vendor_name', $user_id);
-			$vehicle_ids = array_column($this->db->get()->result_array(), 't_vechicle');
+			// Get vehicle IDs owned by vendor
+			$this->db->select('v_id');
+			$this->db->from('vehicles');
+			$this->db->where('v_vendor_name', $user_id);
+			$vehicle_ids = array_column($this->db->get()->result_array(), 'v_id');
 		}
-
 
 		// ---- Trips ----
 		$this->db->select("DATE(trips.t_start_date) AS start, DATE(trips.t_end_date) AS end, trips.t_trip_fromlocation, trips.t_trip_tolocation, CONCAT(vehicles.v_registration_no, '-', vehicles.v_name) AS title, 'green' AS color");
@@ -65,6 +64,7 @@ class Vehicleavailablity extends CI_Controller
 		$this->db->from('vehicle_maintenance vm');
 		$this->db->join('vehicles', 'vm.m_v_id = vehicles.v_id');
 
+		// Filter by accessible vehicles
 		if (!empty($vehicle_ids)) {
 			$this->db->where_in('vm.m_v_id', $vehicle_ids);
 		}

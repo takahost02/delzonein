760223e3e934fe452b5f587e7abd2392a0aa2173
takahost02 <takahost02@gmail.com>
2025-07-22 14:45:17 +0530

    i

--- application/views/vehicle_availability.php
+++ application/views/vehicle_availability.php
@@ -4,6 +4,9 @@
           <div class="col-sm-6">
             <h1 class="m-0 text-dark">Vehicle Calendar
             </h1>
+            <p>User Type: <?= $login_user_type; ?></p>
+            <p>User ID: <?= $login_user_id; ?></p>
+
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">

297ab1f38154d3137ffd28baa4ce28810520614b
takahost02 <takahost02@gmail.com>
2025-07-22 14:42:26 +0530

    i

--- application/controllers/Vehicleavailablity.php
+++ application/controllers/Vehicleavailablity.php
@@ -15,71 +15,70 @@ class Vehicleavailablity extends CI_Controller
 		$user_type = $this->session->userdata('u_user_type');
 		$user_id   = $this->session->userdata('u_id');
 
-		// Filter condition
+		$vehicle_ids = [];
+
+		// Determine accessible vehicle IDs based on user type
 		if ($user_type == 'driver') {
-			$this->db->select('trips.t_vechicle');
+			$this->db->select('t_vechicle');
 			$this->db->from('trips');
-			$this->db->where('trips.t_driver', $user_id);
+			$this->db->where('t_driver', $user_id);
 			$vehicle_ids = array_column($this->db->get()->result_array(), 't_vechicle');
 		} elseif ($user_type == 'vendor') {
-			$this->db->select("v_id");
-			$this->db->from("vehicles");
-			$this->db->where("v_vendor_name", $user_id);
-			$veh_res = $this->db->get()->result_array();
-			$vehicle_ids = array_column($veh_res, 'v_id');
-		} else {
-			$vehicle_ids = [];
+			$this->db->select('v_id');
+			$this->db->from('vehicles');
+			$this->db->where('v_vendor_name', $user_id);
+			$vehicle_ids = array_column($this->db->get()->result_array(), 'v_id');
 		}
 
-		// Trips
-		$this->db->select("date(trips.t_start_date) as start,date(trips.t_end_date) as end,trips.t_trip_fromlocation,trips.t_trip_tolocation,CONCAT(vehicles.v_registration_no, '-', vehicles.v_name) as title,'green' as color");
+		// ---- Trips ----
+		$this->db->select("DATE(trips.t_start_date) AS start, DATE(trips.t_end_date) AS end, trips.t_trip_fromlocation, trips.t_trip_tolocation, CONCAT(vehicles.v_registration_no, '-', vehicles.v_name) AS title, 'green' AS color");
 		$this->db->from('trips');
-		$this->db->join('vehicles', 'trips.t_vechicle=vehicles.v_id');
+		$this->db->join('vehicles', 'trips.t_vechicle = vehicles.v_id');
 
+		// Filter by accessible vehicles
 		if (!empty($vehicle_ids)) {
-			$this->db->where_in('vehicles.v_id', $vehicle_ids);
+			$this->db->where_in('trips.t_vechicle', $vehicle_ids);
+		}
+
+		// Extra filter for driver
+		if ($user_type == 'driver') {
+			$this->db->where('trips.t_driver', $user_id);
 		}
 
 		$query = $this->db->get();
 		$vechdata = $query->result_array();
 
-		$newdata = array();
+		$trips = [];
 		if (!empty($vechdata)) {
 			foreach ($vechdata as $key => $vdata) {
-				$newdata[$key] = $vdata;
-				$newdata[$key]['title'] = $vdata['title'] . ' [' . $vdata['t_trip_fromlocation'] . ' to ' . $vdata['t_trip_tolocation'] . '] ';
-				unset($newdata[$key]['t_trip_fromlocation']);
-				unset($newdata[$key]['t_trip_tolocation']);
+				$trips[$key] = $vdata;
+				$trips[$key]['title'] = $vdata['title'] . ' [' . $vdata['t_trip_fromlocation'] . ' to ' . $vdata['t_trip_tolocation'] . ']';
+				unset($trips[$key]['t_trip_fromlocation'], $trips[$key]['t_trip_tolocation']);
 			}
-			$trips = $newdata;
-		} else {
-			$trips = array();
 		}
 
-		// Maintenance
-		$this->db->select("date(vm.m_start_date) as start,date(vm.m_end_date) as end,CONCAT(vehicles.v_registration_no, '-', vehicles.v_name) as title,'red' as color,vm.m_service_info");
+		// ---- Vehicle Maintenance ----
+		$this->db->select("DATE(vm.m_start_date) AS start, DATE(vm.m_end_date) AS end, CONCAT(vehicles.v_registration_no, '-', vehicles.v_name) AS title, 'red' AS color, vm.m_service_info");
 		$this->db->from('vehicle_maintenance vm');
-		$this->db->join('vehicles', 'vm.m_v_id=vehicles.v_id');
+		$this->db->join('vehicles', 'vm.m_v_id = vehicles.v_id');
 
 		if (!empty($vehicle_ids)) {
-			$this->db->where_in('vehicles.v_id', $vehicle_ids);
+			$this->db->where_in('vm.m_v_id', $vehicle_ids);
 		}
 
 		$query = $this->db->get();
 		$vmresult = $query->result_array();
 
+		$vehicle_maintenance = [];
 		if (!empty($vmresult)) {
-			foreach ($vmresult as $keys => $vm) {
-				$vmnewdata[$keys] = $vm;
-				$vmnewdata[$keys]['title'] = 'Maintenance : ' . $vm['title'] . ' [' . $vm['m_service_info'] . '] ';
-				unset($vmnewdata[$keys]['m_service_info']);
+			foreach ($vmresult as $key => $vm) {
+				$vehicle_maintenance[$key] = $vm;
+				$vehicle_maintenance[$key]['title'] = 'Maintenance: ' . $vm['title'] . ' [' . $vm['m_service_info'] . ']';
+				unset($vehicle_maintenance[$key]['m_service_info']);
 			}
-
-			$vehicle_maintenance = $vmnewdata;
-		} else {
-			$vehicle_maintenance = array();
 		}
 
+		// Merge trips and maintenance
 		$data['vechavail'] = array_merge($trips, $vehicle_maintenance);
 		$this->template->template_render('vehicle_availability', $data);
 	}

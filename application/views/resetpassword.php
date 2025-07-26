c05dd52231d03a2b9fe18eace8a0d51d25fd4417
takahost02 <takahost02@gmail.com>
2025-07-26 17:29:47 +0530

    i

--- application/views/resetpassword.php
+++ application/views/resetpassword.php
@@ -2,13 +2,13 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Change Admin Password
+            <h1 class="m-0 text-dark">Change Password
             </h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="<?= base_url(); ?>/dashboard">Home</a></li>
-              <li class="breadcrumb-item active">Change Admin Password</li>
+              <li class="breadcrumb-item active">Change Password</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
